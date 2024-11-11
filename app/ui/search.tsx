265ad6b1f2b7b3b439154b798a71bf588ff0e7e8
Jhondeser <jhonatan.mog2829@gmail.com>
2024-11-11 16:58:02 +0100

    creamos un buscador

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,7 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
+import {useDebouncedCallback} from 'use-debounce'
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -10,10 +10,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const {replace} = useRouter();
 
-  function handleSearch(term: string){
+  
+  const handleSearch = useDebouncedCallback((term) =>{
     console.log(`buscando ${term}`);
-    
-
     const params = new URLSearchParams();
     if(term){
       params.set('query',term);
@@ -21,7 +20,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query')
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  },300);
+
+  
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
